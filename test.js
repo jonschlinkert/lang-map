81710e7101b022e04ef1343a04a692a67aaa5349
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-15 03:10:43 -0500

    adds tests

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /*!
  * lang-map <https://github.com/jonschlinkert/lang-map>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Copyright (c) 2014-2015, Jon Schlinkert.
  * Licensed under the MIT license.
  */
 
@@ -16,15 +16,20 @@ describe('lang-map', function () {
     assert.equal(typeof languages, 'object');
   });
 
+  it('should return the extensions matching the given language.', function () {
+    assert.deepEqual(languages.ext('asciidoc'), ['asciidoc', 'adoc', 'asc']);
+    assert.deepEqual(languages.ext('python'), ['py', 'gyp', 'lmi', 'pyde', 'pyp', 'pyt', 'pyw', 'wsgi', 'xpy']);
+    assert.deepEqual(languages.ext('markdown'), ['md','markdown','mkd','mkdn','mkdown','ron']);
+  });
+
   it('should return the language matching the given extension.', function () {
+    assert.equal(languages.lang('.md'), 'markdown');
+    assert.equal(languages.lang('md'), 'markdown');
+    assert.equal(languages.lang('mkd'), 'markdown');
+    assert.equal(languages.lang('ron'), 'markdown');
     assert.equal(languages.lang('py'), 'python');
     assert.equal(languages.lang('js'), 'javascript');
     assert.equal(languages.lang('hbs'), 'handlebars');
     assert.equal(languages.lang('.js'), 'javascript');
-  });
-
-  it('should return the extensions matching the given language.', function () {
-    assert.deepEqual(languages.ext('asciidoc'), ["asciidoc", "adoc", "asc"]);
-    assert.deepEqual(languages.ext('python'), ["py", "gyp", "lmi", "pyde", "pyt", "pyw", "wsgi", "xpy"]);
   });
 });
