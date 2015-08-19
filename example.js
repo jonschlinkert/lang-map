c0813ee0523aa131b7ab84d599ae074be4051b47
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-18 22:15:19 -0400

    adds examples

---
+++ example.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var map = require('./');
+
+console.log("map.extensions('coffee');");
+console.log(map.extensions('coffee'));
+console.log("map.extensions('hbs');");
+console.log(map.extensions('hbs'));
+console.log("map.extensions('markdown');");
+console.log(map.extensions('markdown'));
+console.log("map.extensions('md');");
+console.log(map.extensions('md'));
+console.log("map.extensions('c');");
+console.log(map.extensions('c'));
+
+console.log("map.languages('coffee');");
+console.log(map.languages('coffee'));
+console.log("map.languages('handlebars');");
+console.log(map.languages('handlebars'));
+console.log("map.languages('hbs');");
+console.log(map.languages('hbs'));
+console.log("map.languages('javascript');");
+console.log(map.languages('javascript'));
+console.log("map.languages('js');");
+console.log(map.languages('js'));
+console.log("map.languages('md');");
+console.log(map.languages('md'));
+console.log("map.languages('mkdown');");
+console.log(map.languages('mkdown'));
+console.log("map.languages('h');");
+console.log(map.languages('h'));
