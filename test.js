88710b6e798787c6e0dd4210cc0a407a9c3e9df0
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-18 22:21:48 -0400

    update tests for 0.4.0 API

--- test.js
+++ test.js
@@ -7,29 +7,36 @@
 
 'use strict';
 
-var languages = require('./');
+var map = require('./');
 var assert = require('assert');
 
-describe('lang-map', function () {
+function containEqual(a, b) {
+  var len = a.length;
+  while (len--) if (!~b.indexOf(a[len])) return false;
+  return true;
+}
 
+describe('lang-map', function () {
   it('should return the full list of languages.', function () {
+    var languages = map().languages;
     assert.equal(typeof languages, 'object');
   });
 
   it('should return the extensions matching the given language.', function () {
-    assert.deepEqual(languages.ext('asciidoc'), ['asciidoc', 'adoc', 'asc']);
-    assert.deepEqual(languages.ext('python'), ['py','gyp','lmi','pyde','pyp','pyt','pyw','wsgi','xpy']);
-    assert.deepEqual(languages.ext('markdown'), ['md','markdown','mkd','mkdn','mkdown','ron']);
+    assert(containEqual(['asciidoc', 'adoc', 'asc'], map.extensions('asciidoc')));
+    assert(containEqual(['py','gyp','lmi','pyde','pyp','pyt','pyw','wsgi','xpy'], map.extensions('python')));
+    assert(containEqual(['md','markdown','mkd','mkdn','mkdown','ron'], map.extensions('markdown')));
   });
 
   it('should return the language matching the given extension.', function () {
-    assert.equal(languages.lang('.md'), 'markdown');
-    assert.equal(languages.lang('md'), 'markdown');
-    assert.equal(languages.lang('mkd'), 'markdown');
-    assert.equal(languages.lang('ron'), 'markdown');
-    assert.equal(languages.lang('py'), 'python');
-    assert.equal(languages.lang('js'), 'javascript');
-    assert.equal(languages.lang('hbs'), 'handlebars');
-    assert.equal(languages.lang('.js'), 'javascript');
+    assert.deepEqual(map.languages('.md'), ['markdown']);
+    assert.deepEqual(map.languages('markdown'), ['markdown']);
+    assert.deepEqual(map.languages('md'), ['markdown']);
+    assert.deepEqual(map.languages('mkd'), ['markdown']);
+    assert.deepEqual(map.languages('ron'), ['markdown']);
+    assert.deepEqual(map.languages('py'), ['python']);
+    assert.deepEqual(map.languages('js'), ['javascript']);
+    assert.deepEqual(map.languages('hbs'), ['handlebars']);
+    assert.deepEqual(map.languages('.js'), ['javascript']);
   });
 });
